188e8c21d6a83da9bf2b53c99cc8fb9f442729c0
DameSeven <98630693+DameSeven@users.noreply.github.com>
2023-07-26 00:15:26 +0800

    Update TestClass2.java
    push2

--- src/main/java/com/tplink/test/TestClass2.java
+++ src/main/java/com/tplink/test/TestClass2.java
@@ -16,5 +16,6 @@ public class TestClass2 {
         System.out.println("hot-fix");
         System.out.println("hot-fix");
         System.out.println("push");
+        System.out.println("push2");
     }
 }
